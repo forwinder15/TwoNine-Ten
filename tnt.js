5538d52b1e2a412bf0afca257e6ff938c7f55942
forwinder15 <jrogercoker@gmail.com>
2018-08-14 22:53:44 -0400

    Added more imgs and made adustment to nav and parallax

--- tnt.js
+++ tnt.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-      $(window).on('scroll', function () {
-        if (Math.round($(window).scrollTop()) > 100) {
-          $('.navbar').addClass('scrolled');
-        } else {
-          $('.navbar').removeClass('scrolled');
-        }
-      });
-    });
+$(document).ready(function() {
+  $(window).on('scroll', function() {
+    if (Math.round($(window).scrollTop()) > 100) {
+      $('.navbar').addClass('scrolled');
+    } else {
+      $('.navbar').removeClass('scrolled');
+    }
+  });
+});
